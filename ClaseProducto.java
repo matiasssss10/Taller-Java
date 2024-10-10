b1366a68378e14fb340014450786ac1becae972b
matiasssss10 <danielmatiasmendozatolosa@gmail.com>
2024-10-10 09:27:32 -0500

    Create ClaseProducto.java

---
+++ ClaseProducto.java
@@ -0,0 +1,36 @@
+public class Producto {
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                '}';
+    }
+}
